593762794f15f65fc76a7b1c71be95284343b9d2
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.4>
2024-12-10 16:00:57 -0300

    feat: product action menu

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -29,6 +29,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 import { catchError, finalize, throwError } from 'rxjs';
 import { DeleteProductModalComponent } from '../components/delete-product-modal/delete-product-modal.component';
@@ -78,6 +79,7 @@ export enum EProductType {
     MatSelectModule,
     FormsModule,
     ReactiveFormsModule,
+    MatMenuModule,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
